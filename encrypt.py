9c1347ab04880c4d1902ef3a8767bdfb152d55a4
Roberto Galvez Meyer <139347219+RobertoGM5@users.noreply.github.com>
2023-09-01 11:57:31 +0200

    Added both programs

---
+++ encrypt.py
@@ -0,0 +1,28 @@
+plain_text = input("What is the message?: ")
+shift_key = int(input("What is the key?: "))
+
+cipher_text = ''
+
+    
+for letter in plain_text:
+    if letter == " ":
+        cipher_text += "¶"
+    elif letter.isupper():
+        char_position = ord(letter)
+        new_position = ((char_position + shift_key - 65) % 26) + 65
+        new_letter = chr(new_position)
+        cipher_text += new_letter
+    elif letter.islower():
+        char_position = ord(letter)
+        new_position = ((char_position + shift_key - 97) % 26) + 97
+        new_letter = chr(new_position)
+        cipher_text += new_letter
+    elif letter.isnumeric():
+        char_position = ord(letter)
+        new_position = ((char_position + shift_key - 48) % 10) + 48
+        new_letter = chr(new_position)
+        cipher_text += new_letter
+    else:
+        cipher_text += letter
+
+print(cipher_text)
